c92ea053c2f015c2b9ae167946b462aa3b5b7ef0
root <root@localhost>
2023-08-04 02:32:53 -0600

    Delete files

--- include/generated/compile.h
+++
@@ -1,4 +0,0 @@
-#define UTS_MACHINE		"x86_64"
-#define LINUX_COMPILE_BY	"root"
-#define LINUX_COMPILE_HOST	"node0.hansens.nestfarm-pg0.wisc.cloudlab.us"
-#define LINUX_COMPILER		"gcc (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0, GNU ld (GNU Binutils for Ubuntu) 2.34"
--- include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "6.1.0-g7cddc2c31b92-dirty"
